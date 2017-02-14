1cfc4c656652f23161a0c987c989cec1b1743110
Walter A. Alves <walter@macbook.local>
2017-02-13 23:37:12 -0200

    Initial commit
    This commit starts the project with a Spring boot project and Maven basic configurations.

---
+++ src/test/java/nasa/robot/challenge/NasaRobotApplicationTests.java
@@ -0,0 +1,16 @@
+package nasa.robot.challenge;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NasaRobotApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
