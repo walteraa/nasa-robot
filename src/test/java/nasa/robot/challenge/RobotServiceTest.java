b8b7ca47dec8f13c79c857d7bdc3ecdd05394751
Walter A. Alves <walter@macbook.local>
2017-02-14 00:07:30 -0200

    Add basic implementation to Service layer
    Addind the basic setup and a (not passing) test case to RobotService.

--- src/test/java/nasa/robot/challenge/RobotServiceTest.java
+++ src/test/java/nasa/robot/challenge/RobotServiceTest.java
@@ -1,6 +1,11 @@
 package nasa.robot.challenge;
 
+import static org.junit.Assert.*;
+
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -8,10 +13,21 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration
 public class RobotServiceTest {
+
+	@Configuration
+	static class RobotServiceTestConfiguration {
+		@Bean
+		public RobotService robotService() {
+			return new RobotService();
+		}
+	}
 	
-@Configuration
-static class RobotServiceTestConfiguration{
+	@Autowired
+	private RobotService robotService;
 	
-}
-	
+	@Test
+	public void testBasicInvalidMove(){
+		fail();
+	}
+
 }
