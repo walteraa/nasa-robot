cc40402d065ea6a5d1a6bc1126478678feac0d33
Walter A. Alves <walter@macbook.local>
2017-02-13 23:59:04 -0200

    Add Robot service Test
    Adding the file to implement TTD to Robot Service.
    This Test file will test service layer.

---
+++ src/test/java/nasa/robot/challenge/RobotServiceTest.java
@@ -0,0 +1,17 @@
+package nasa.robot.challenge;
+
+import org.junit.runner.RunWith;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration
+public class RobotServiceTest {
+	
+@Configuration
+static class RobotServiceTestConfiguration{
+	
+}
+	
+}
